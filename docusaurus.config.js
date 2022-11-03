1974d65b3f14c7bb24f90b09f08d7dfafdfd498b
chinmaysomani <chinmaysomani7@gmail.com>
2022-11-03 13:29:32 +0530

    change title and desc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Tutorial',
-  tagline: 'This tutorial is cool.',
+  title: 'My Tutorial.......',
+  tagline: 'This tutorial is cool.......',
   url: 'https://github.com',
   baseUrl: '/my-dokku-website/',
   onBrokenLinks: 'throw',
